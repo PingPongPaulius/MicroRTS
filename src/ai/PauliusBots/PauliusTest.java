cb896ea6bfba3f0d87149b6be1cc6b9700faffd3
PingPongPaulius <kothedragoon@gmail.com>
2024-05-26 13:02:44 +0100

    Player Move choise checkpoint.

--- src/ai/PauliusBots/PauliusTest.java
+++ src/ai/PauliusBots/PauliusTest.java
@@ -5,9 +5,15 @@ import ai.core.AIWithComputationBudget;
 import ai.core.ParameterSpecification;
 import rts.GameState;
 import rts.PlayerAction;
+import rts.PlayerActionGenerator;
+import rts.UnitAction;
+import rts.units.Unit;
 import rts.units.UnitTypeTable;
+import util.Pair;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 public class PauliusTest extends AIWithComputationBudget {
@@ -20,17 +26,24 @@ public class PauliusTest extends AIWithComputationBudget {
     UnitTypeTable m_utt = null;
     // This is the default constructor that microRTS will call:
 
+    /*
+    Strategy is to pick best action and rank it.
+     */
+
+    HashMap<Long, HashSet<UnitAction>> actionRanks;
+
     public PauliusTest(UnitTypeTable utt) {
 
         super(-1,-1);
         m_utt = utt;
+
+        actionRanks = new HashMap<>();
 
     }
 
     // This will be called by microRTS when it wants to create new instances of this bot (e.g., to play multiple games).
 
     public AI clone() {
-
         return new PauliusTest(m_utt);
     }
 
@@ -43,13 +56,53 @@ public class PauliusTest extends AIWithComputationBudget {
     // Called by microRTS at each game cycle.
     // Returns the action the bot wants to execute.
 
-    public PlayerAction getAction(int player, GameState gs) {
-        PlayerAction pa = new PlayerAction();
-        pa.fillWithNones(gs, player, 10);
-        return pa;
+    public PlayerAction getAction(int player, GameState gs) throws Exception {
+
+        PlayerAction playerAction = new PlayerAction();
+
+        if (!gs.canExecuteAnyAction(player)) return new PlayerAction();
+
+        PlayerActionGenerator actionGenerator = new PlayerActionGenerator(gs, player);
+        List<Pair<Unit,List<UnitAction>>> possibleMoves = actionGenerator.getChoices();
+
+        for(Pair<Unit, List<UnitAction>> pair: possibleMoves) {
+
+            Unit unit = pair.m_a;
+            List<UnitAction> possibleUnitActions = pair.m_b;
+
+            boolean unitHasAction = false;
+
+            if (actionRanks.containsKey(unit.getID())) {
+                    HashSet<UnitAction> actionsTried = actionRanks.get(unit.getID());
+                    for (UnitAction action : possibleUnitActions) {
+
+                        if(unitHasAction) continue;
+
+                        if (!actionsTried.contains(action)) {
+                            if(unit.canExecuteAction(action, gs)) {
+                                if(!unit.getType().canMove) {
+                                    System.out.println("UNIT: " + unit);
+                                    System.out.println("ACTIONS: " + actionsTried);
+                                    System.out.println("NEXT ACTION: " + action);
+                                }
+                                actionsTried.add(action);
+                                playerAction.addUnitAction(unit, action);
+                                actionRanks.put(unit.getID(), actionsTried);
+                                unitHasAction = true;
+                            }
+                        }
+                    }
+            } else {
+                PlayerAction action = actionGenerator.getRandom();
+                playerAction = playerAction.merge(action);
+                actionRanks.put(unit.getID(), new HashSet<>(List.of(action.getAction(unit))));
+            }
+
+        }
+
+        return playerAction;
 
     }
-
     // This will be called by the microRTS GUI to get the
     // list of parameters that this bot wants exposed
     // in the GUI.
